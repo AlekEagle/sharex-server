9bbf69d8b7a7d096df54e55ccd09027cd94259f3
Alek Evans <alek@lugetron.com>
2019-12-22 21:40:40 -0700

    serviceworker added

---
+++ root/sw.js
@@ -0,0 +1,54 @@
+var CACHE_NAME = 'alekeagle-me-v1';
+var urlsToCache = [
+    '/',
+    '/assets/css/universal.css',
+    '/assets/js/memory.js',
+    '/assets/images/circle.png',
+    '/assets/images/me_irl.png',
+    '/assets/images/empty.gif'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic' || response.url.includes('/api/')) {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
