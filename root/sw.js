d050ab0e392135ced643360fb39fea000dde8029
Alek Evans <alek@lugetron.com>
2019-12-23 16:01:09 -0700

    fuck

--- root/sw.js
+++ root/sw.js
@@ -32,6 +32,15 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
