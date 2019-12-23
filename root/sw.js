28da829fabc0241cca89f38bbbb16ac6616b6156
Alek Evans <alek@lugetron.com>
2019-12-23 14:25:25 -0700

    lmao

--- root/sw.js
+++ root/sw.js
@@ -28,6 +28,20 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName === CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+})
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
